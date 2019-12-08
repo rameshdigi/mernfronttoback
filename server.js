6d00e2569ab25d280f3f62c6d002b2f5a892bf02
ramesh <rameshdigi@gmail.com>
2019-12-08 14:57:26 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+//connect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/post'));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
